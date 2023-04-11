f8818140289bf73396ef317440a84ffc70076a2c
Riecho Pinanonang <chong2xrhp@gmail.com>
2023-04-11 22:48:52 +0800

    First Program 1

---
+++ src/main/kotlin/KotlinPersonalInfo.kt
@@ -0,0 +1,38 @@
+fun main(){
+    //Personal Information
+    var position:String = "Mobile Developer"
+    var firstName:String = "Riecho"
+    var lastName:String = "Pinanonang"
+    var middleName:String = "Hernandez"
+    var address:String = "Rizal Street, Tinago"
+    var city:String = "Bayawan City"
+    var state:String = "Phil"
+    var zipCode:String = "6221"
+    var homePhone:String = "(035) 430-8478"
+    var cellPhone:String = "0917 305 3158"
+    var emailAddress:String = "chong2xrhp@gmail.com"
+    var sssNumber:String = "123456780"
+    var isUSCitizen:String = "No"
+    var isConvictedFelony:String = "No"
+
+    println("First Name: $firstName")
+    println("Middle Name: $middleName")
+    println("Last Name: $lastName")
+    println("Address : $address")
+    println("City: $city")
+    println("State: $state")
+    println("Zip Code: $zipCode")
+    println("Home Phone: $homePhone")
+    println("Cell Phone: $cellPhone")
+    println("Email Address: $emailAddress")
+    println("sssNumber: $sssNumber")
+    println("Are you a U.S. Citizen?: $isUSCitizen")
+    println("Have you ever been convicted of a felony?: $isConvictedFelony")
+
+    //Education
+    var schoolName:String = "NORSU"
+    var schoolLocation:String = "Negros Oriental"
+    var yearsAttended:String = "2000"
+
+
+}
---
+++ src/main/kotlin/KotlinProgram3.kt
@@ -0,0 +1,16 @@
+fun main(){
+    //implicit variable declaration
+    //var name = "Riecho"
+    //the declaration is similar to -> var name:String = "Riecho"
+
+    //Input - Process - Output
+    //for String input
+    println("Enter your name: ")
+    var name :String = readln()
+    println("Hello $name!!")
+
+    //for Integer input
+    println("Enter a number: ")
+    var number : Int = readln().toInt()
+    println("You have entered the number $number!")
+}
