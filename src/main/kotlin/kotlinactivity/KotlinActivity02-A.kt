cd3fc845a1a4f7aa1a4f1cc72845915c1fee71b7
Riecho Pinanonang <chong2xrhp@gmail.com>
2023-05-08 16:37:05 +0800

    Commit Kotlin activity 02-A

--- src/main/kotlin/kotlinactivity/KotlinActivity02-A.kt
+++ src/main/kotlin/kotlinactivity/KotlinActivity02-A.kt
@@ -7,45 +7,58 @@ fun main(){
     do{
         showMenu() //Show menu option
         option = readln().first()
+        println()
 
         when(option){
             'a' -> students.add(studentAdd())
             'b' -> studentList(students)
         }
 
-    } while(option != 'c')
+    } while(option != 'x')
 
 }
 
 fun studentList(list:MutableList<List<Any>>){
+    for(i in 0..list.size -1){
+         if(list[i].size -1 > 0){
+             println("$i. Name: ${list[i][0]} ${list[i][1]} ${list[i][2]}" +
+                     ", Address: ${list[i][3]}, Birth date: ${list[i][4]}" +
+                     ", Course: ${list[i][5]}, Email: ${list[i][6]}" +
+                     ", Contact No.: ${list[i][7]}")
+         }
+    }
+    println()
 
 }
+
 fun showMenu(){
-    println("*** OPTION ***")
-    println("a. Add student")
-    println("b. List student")
-    println("c. Exit option")
+    println("**** OPTION ****")
+    println("[a] Add student")
+    println("[b] List student")
+    println("[x] Exit option")
+    print("Enter you option: ")
 }
 
 fun studentAdd():MutableList<Any>{
     var student = mutableListOf<Any>()
-    println("Enter first name: ")
+    print("Enter first name: ")
     var firstName = readln()
-    println("Enter middle name: ")
+    print("Enter middle name: ")
     var middleName = readln()
-    println("Enter last name: ")
+    print("Enter last name: ")
     var lastName = readln()
-    println("Address: ")
+    print("Address: ")
     var address = readln()
-    println("Birth Date: ")
+    print("Birth Date: ")
     var birthdate = readln()
-    println("Course: ")
+    print("Course: ")
     var course = readln()
-    println("Email address: ")
+    print("Email address: ")
     var emailAdd = readln()
-    println("Enter mobile no.: ")
+    print("Enter mobile no.: ")
     var mobilenumber = readln()
 
+    println()
     student.add(firstName)
     student.add(middleName)
     student.add(lastName)
